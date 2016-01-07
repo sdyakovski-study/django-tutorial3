15089e6ec4216a8596a1d189276f3eee0cfe33be
Svilen Dyakovski <sdyakovski@gmail.com>
2016-01-06 23:26:02 -0800

    Added BASE_PATH, PROJECT_PATH and their descr.

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ I changed the project structure a bit compared to the one suggested in the book:
 - then cd project && mv project tutorialsite
 - now I have in django-tutorial3 a folder project which has tutorialsite and manage.py. Added in here this README.md
 
-- In my other study projects I had called startproject myproject, which would create myproject/myproject and then would rename the outer myproject to project. It worked without a problem because Django (in manage.py, myproject/settings.py and myproject/wsgl.py) only cares how the inner folder is called. THat folder is the project's Django package. 
-But this time I renamed the inner one - from project to tutorialsite. Then I had to go in the 3 py files and manually chnaged the reference from project to tutorialsite.
+- In my other study projects I had called startproject myproject, which would create myproject/myproject and then would rename the outer myproject to project.
+ It worked without a problem because Django (in manage.py, myproject/settings.py and myproject/wsgl.py) only cares how the inner folder is called. THat folder is the project's Django package. 
+ But this time I renamed the inner one - from project to tutorialsite. Then I had to go in the 3 py files and manually chnaged the reference from project to tutorialsite.
 - Now python manage.py runserver works and starts the Development Server.
--- tutorialsite/settings.py
+++ tutorialsite/settings.py
@@ -10,7 +10,29 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# settings.py will be read and evaluated on the project level (possibly by manage.py).
+# BASE_DIR points to the '/home/sdyakovski/projects/python-projects/django-tutorial3/project'
+# but is dealing with relative paths, not absolute paths, so its value from manage.py would be ''.
+# It simply starts with __file__ (which is settings.py); its dirname (tutorialsite); and its dirname (project),
+# which is the curdir of manage.py => '' 
+
+# ADD EXTERNALS PATHS TO sys.path
+BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# BASE_PATH is abspath of 'tutorialsite/..', which is 
+# '/home/sdyakovski/projects/python-projects/django-tutorial3/project'
+# - which is the same dir as BASE_DIR, but absolute (needed for sys.path)
+EXTERNAL_LIBS_PATH = os.path.join(BASE_PATH, 'externals', 'libs')
+EXTERNAL_APPS_PATH = os.path.join(BASE_PATH, 'externals', 'apps')
+sys.path = ['', EXTERNAL_LIBS_PATH, EXTERNAL_APPS_PATH] + sys.path[1:]
+# [1:] in order to eliminate the first element of the list (''), which is moved to the front
+print sys.path
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+# PROJECT_PATH is abspath of 'tutorialsite', where __file__ (settings.py) is, which is 
+# '/home/sdyakovski/projects/python-projects/django-tutorial3/project/tutorialsite'
 
 
 # Quick-start development settings - unsuitable for production
@@ -67,7 +89,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
